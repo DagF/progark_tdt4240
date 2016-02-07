6d819a2373fc8af33fa306730c323ceaea1e8033
Dag Frode <tncditto@gmail.com>
2016-02-07 07:33:24 +0100

    Done with task 2

--- technology_intro/TechnologyIntro/app/src/main/java/de/dagfro/technologyintro/Task2Activity.java
+++ technology_intro/TechnologyIntro/app/src/main/java/de/dagfro/technologyintro/Task2Activity.java
@@ -1,10 +1,12 @@
 package de.dagfro.technologyintro;
 
+import android.graphics.Point;
 import android.support.v4.view.VelocityTrackerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
+import android.view.View;
 
 /*
 * TASK 2 – Input and text
@@ -19,55 +21,16 @@ b) Print the position of the sprite (in screen coordinates). The text should be 
 
 public class Task2Activity extends AppCompatActivity {
 
+    Task2View view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new Task2View(this));
+        view = new Task2View(this);
+        setContentView(view);
     }
 
     private static final String DEBUG_TAG = "Velocity";
     private VelocityTracker mVelocityTracker = null;
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int index = event.getActionIndex();
-        int action = event.getActionMasked();
-        int pointerId = event.getPointerId(index);
-
-        switch(action) {
-            case MotionEvent.ACTION_DOWN:
-                if(mVelocityTracker == null) {
-                    // Retrieve a new VelocityTracker object to watch the velocity of a motion.
-                    mVelocityTracker = VelocityTracker.obtain();
-                }
-                else {
-                    // Reset the velocity tracker back to its initial state.
-                    mVelocityTracker.clear();
-                }
-                // Add a user's movement to the tracker.
-                mVelocityTracker.addMovement(event);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                mVelocityTracker.addMovement(event);
-                // When you want to determine the velocity, call
-                // computeCurrentVelocity(). Then call getXVelocity()
-                // and getYVelocity() to retrieve the velocity for each pointer ID.
-                mVelocityTracker.computeCurrentVelocity(1000);
-                // Log velocity of pixels per second
-                // Best practice to use VelocityTrackerCompat where possible.
-                System.out.println("X velocity: " +
-                        VelocityTrackerCompat.getXVelocity(mVelocityTracker,
-                                pointerId));
-                System.out.println( "Y velocity: " +
-                        VelocityTrackerCompat.getYVelocity(mVelocityTracker,
-                                pointerId));
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                // Return a VelocityTracker object back to be re-used by others.
-                mVelocityTracker.recycle();
-                break;
-        }
-        return true;
-    }
 }
--- technology_intro/TechnologyIntro/app/src/main/java/de/dagfro/technologyintro/Task2View.java
+++ technology_intro/TechnologyIntro/app/src/main/java/de/dagfro/technologyintro/Task2View.java
@@ -6,7 +6,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -19,12 +22,20 @@ public class Task2View extends SurfaceView {
     private SurfaceHolder holder;
     private Task2Thread task2Thread;
     private int x = 0;
+    private int y = 0;
     private int xSpeed = 10;
+    private Point finger;
+    private int ySpeed = 10;
+    private float mLastTouchX;
+    private float mLastTouchY;
+    private float lastAngle = 90;
+    private int direction = -1;
 
     public Task2View(Context context) {
         super(context);
         task2Thread = new Task2Thread(this);
         holder = getHolder();
+
         holder.addCallback(new SurfaceHolder.Callback() {
 
             @Override
@@ -66,18 +77,122 @@ public class Task2View extends SurfaceView {
         return dst;
     }
 
+    public void setFinger(Point finger){
+        finger = finger;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        final int action = ev.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN: {
+                final float x = ev.getX();
+                final float y = ev.getY();
+
+                // Remember where we started
+                mLastTouchX = x;
+                mLastTouchY = y;
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                final float x = ev.getX();
+                final float y = ev.getY();
+
+                // Calculate the distance moved
+                final float dx = x - mLastTouchX;
+                final float dy = y - mLastTouchY;
+
+                // Move the object
+                float mPosX = dx;
+                float mPosY = dy;
+
+                // Remember this touch position for the next move event
+                mLastTouchX = x;
+                mLastTouchY = y;
+                finger = new Point((int)mLastTouchX,(int)mLastTouchY);
+
+                // Invalidate to request a redraw
+                invalidate();
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    public static float angleBetweenTwoPointsWithFixedPoint(Point point1, Point point2) {
+
+        float angle1 = (float) Math.atan2(point1.y - 0, point1.x - 0);
+        float angle2 = (float) Math.atan2(point2.y - 0, point2.x - 0);
+
+        return angle1 - angle2;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
-        if (x >= getWidth() - bmp.getWidth()) {
-            xSpeed = -10;
-            bmp = flip(bmp);
+        if(canvas != null){
+            if(finger == null){
+                finger = new Point(canvas.getWidth() / 2, canvas.getHeight()/2);
+                x = finger.x;
+                y = finger.y;
+            }
+
+            Point heli = new Point(x,y);
+
+
+
+            if ((x >= getWidth() - bmp.getWidth() && x <= finger.x) || (x <= 0 && x >= finger.x) || (finger.x - 5.0 < x && x < finger.x + 5.0)) {
+                xSpeed = 0;
+            }
+            else{
+                if( x < finger.x){
+                    xSpeed = 10;
+                }
+                else if( x > finger.x){
+                    xSpeed = -10;
+                }
+                else{
+                    xSpeed = 0;
+                }
+            }
+            if ((y >= getHeight() - bmp.getHeight() && y <= finger.y)|| (y <= 0 && y >= finger.y) || (finger.y - 5.0 < y && y < finger.y + 5.0)) {
+                ySpeed = 0;
+            }
+            else{
+                if( y < finger.y){
+                    ySpeed = 10;
+                }
+                else if(y > finger.y) {
+                    ySpeed = -10;
+                }
+                else{
+                    ySpeed = 0;
+                }
+            }
+            x = x + xSpeed;
+            y = y + ySpeed;
+
+            if(x < finger.x && direction == -1){
+                direction = 1;
+                bmp = flip(bmp);
+            }
+            if(x > finger.x && direction == 1){
+                direction = -1;
+                bmp = flip(bmp);
+            }
+
+
+            Matrix m = new Matrix();
+
+            canvas.drawColor(Color.BLACK);
+            canvas.drawBitmap(bmp, x, y, null);
+
+            Paint paint = new Paint();
+
+            paint.setColor(Color.WHITE);
+            paint.setTextSize(20);
+            canvas.drawText("x: " + x + ", y: " + y, 10, 25, paint);
         }
-        if (x <= 0) {
-            xSpeed = 10;
-            bmp = flip(bmp);
-        }
-        x = x + xSpeed;
-        canvas.drawColor(Color.BLACK);
-        canvas.drawBitmap(bmp, x, 10, null);
     }
 }
