722c1acb41c2c2baa2a8dbba6444c9fe6e399f74
Dag Frode <tncditto@gmail.com>
2016-02-07 23:48:33 +0100

    x

--- technology_intro/TechnologyIntro/app/src/main/java/de/dagfro/technologyintro/Task4Ball.java
+++ technology_intro/TechnologyIntro/app/src/main/java/de/dagfro/technologyintro/Task4Ball.java
@@ -1,5 +1,6 @@
 package de.dagfro.technologyintro;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -12,6 +13,11 @@ public class Task4Ball {
     private Point point;
     private int radius;
     private Paint color = new Paint(Color.BLACK);
+
+
+    public void draw(Canvas canvas){
+        canvas.drawCircle(point.x, point.y, radius, color);
+    }
 
     public Task4Ball(Point point, int radius){
 
--- technology_intro/TechnologyIntro/app/src/main/java/de/dagfro/technologyintro/Task4Bar.java
+++ technology_intro/TechnologyIntro/app/src/main/java/de/dagfro/technologyintro/Task4Bar.java
@@ -1,5 +1,6 @@
 package de.dagfro.technologyintro;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -8,10 +9,17 @@ import android.graphics.Point;
  * Created by dagfs on 07-Feb-16.
  */
 public class Task4Bar {
-    private int length= 100;
+    private int height= 100;
     private int width = 15;
     private Paint color = new Paint(Color.BLACK);
     private Point point;
+    private int ySpeed;
+    private int xSpeed;
+
+
+    public void draw(Canvas canvas){
+        canvas.drawRect(point.x, point.y, point.x + width, point.y + height,color);
+    }
 
     public Task4Bar(Point point){
         this.point = point;
